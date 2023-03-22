c27574150f8377f16e282646892d1038212238a3
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-03-22 23:30:04 +0200

    add new arr

--- js/product-jacket-items.js
+++ js/product-jacket-items.js
@@ -1,4 +1,5 @@
-export const productItems = [
+export const jacketArr = [
+  
   {
     ountification: "01",
     preview: './img/product/jacket/black/01.jpg',
@@ -60,3 +61,98 @@ export const productItems = [
     ],
   },
 ];
+
+export const giletArr = [
+  {
+    ountification: "01",
+    preview: './img/product/gilet/black/01.jpg',
+    description: 'gilet-black',
+    name: 'gilet-black',
+    listsPhoto: [
+      './img/product/gilet/black/01.jpg',
+      './img/product/gilet/black/02.jpg',
+      './img/product/gilet/black/03.jpg',
+      './img/product/gilet/black/04.jpg',
+      './img/product/gilet/black/05.jpg',
+    ],
+  },
+  {
+    ountification: "02",
+    preview: './img/product/gilet/milk/01.jpg',
+    description: 'gilet-milk',
+    name: 'gilet-milk',
+    listsPhoto: [
+      './img/product/gilet/milk/01.jpg',
+      './img/product/gilet/milk/02.jpg',
+      './img/product/gilet/milk/03.jpg',
+      './img/product/gilet/milk/04.jpg',
+      './img/product/gilet/milk/05.jpg',
+    ],
+  },
+]
+
+export const  giletLongArr =[
+  {
+    ountification: "01",
+    preview: './img/product/gilet-long/white/01.jpg',
+    description: 'gilet-long-white',
+    name: 'gilet-long-white',
+    listsPhoto: [
+      './img/product/gilet-long/white/01.jpg',
+      './img/product/gilet-long/white/02.jpg',
+      './img/product/gilet-long/white/03.jpg',
+      './img/product/gilet-long/white/04.jpg', 
+    ],
+  },
+  {
+    ountification: "02",
+    preview: './img/product/gilet-long/milk/01.jpg',
+    description: 'gilet-long-milk',
+    name: 'gilet-long-milk',
+    listsPhoto: [
+      './img/product/gilet-long/milk/01.jpg',
+      './img/product/gilet-long/milk/02.jpg',
+      './img/product/gilet-long/milk/03.jpg',
+      './img/product/gilet-long/milk/04.jpg',
+      './img/product/gilet-long/milk/05.jpg',
+    ],
+  },
+]
+export const  tracksuitArr =[
+  {
+    ountification: "01",
+    preview: './img/product/tracksuit/black/01.jpg',
+    description: 'tracksuit-black',
+    name: 'tracksuit-black',
+    listsPhoto: [
+      './img/product/tracksuit/black/01.jpg',
+      './img/product/tracksuit/black/02.jpg',
+      './img/product/tracksuit/black/03.jpg',
+      './img/product/tracksuit/black/04.jpg', 
+    ],
+  },
+  {
+    ountification: "02",
+    preview: './img/product/tracksuit/black-modal2/01.jpg',
+    description: 'tracksuit-black-modal2',
+    name: 'tracksuit-black-modal2',
+    listsPhoto: [
+      './img/product/tracksuit/black-modal2/01.jpg',
+      './img/product/tracksuit/black-modal2/02.jpg',
+      './img/product/tracksuit/black-modal2/03.jpg',
+    ],
+  },
+  {
+    ountification: "03",
+    preview: './img/product/tracksuit/gray/01.jpg',
+    description: 'tracksuit-gray',
+    name: 'tracksuit-gray',
+    listsPhoto: [
+      './img/product/tracksuit/gray/01.jpg',
+      './img/product/tracksuit/gray/02.jpg',
+      './img/product/tracksuit/gray/03.jpg',
+      './img/product/tracksuit/gray/04.jpg',
+      './img/product/tracksuit/gray/05.jpg',
+    ],
+  },
+]
